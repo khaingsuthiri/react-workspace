4d9f662730dd223ae2cd636a5df6a1bf91efa005
Khaing Su Thiri <khaingsuthiri@gmail.com>
2023-09-27 19:17:42 +0630

    [Modified] Toolbar

--- first/src/Toolbar.js
+++ first/src/Toolbar.js
@@ -2,9 +2,6 @@ import React from "react";
 
 class Toolbar extends React.Component {
   render() {
-    let parent = 200;
-    let height = 150;
-
     const styles = {
       toolbar: {
         border: "10px solid pink",
