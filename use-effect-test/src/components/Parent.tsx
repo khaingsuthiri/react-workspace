b4c839976c3c2c52d31b27aa7266905641d6fd12
khaingsuthiri <khaingsuthiri@gmail.com>
2026-02-24 15:55:35 +0630

    learning: enhance Parent component with useMemo for name calculation and update handlePrint function

--- use-effect-test/src/components/Parent.tsx
+++ use-effect-test/src/components/Parent.tsx
@@ -1,12 +1,18 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Child from "./Child";
 
 export default function Parent() {
   const [count, setCount] = useState(0);
+  const [lastName, setLastName] = useState("Pont");
+
+  const name = useMemo(() => {
+    return "Pont " + lastName;
+  }, [lastName]);
 
   const handlePrint = useCallback(() => {
     console.log("Hello Pont!");
-  }, []);
+    console.log("Real Name: ", name);
+  }, [name]);
 
   //   function handlePrint() {
   //     console.log("Hello Pont!");
