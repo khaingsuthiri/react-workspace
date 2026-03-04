a0fe33aa4a67fd5425aa37525cc8efbe194219e6
khaingsuthiri <khaingsuthiri@gmail.com>
2026-03-04 14:21:01 +0630

    learning: enhance App component to include reset functionality with Key

--- context-app/src/App.tsx
+++ context-app/src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Profile from "./components/Profile";
 import UserProvider from "./contexts/UserContextReducer";
 
 function App() {
+  const [resetKey, setResetKey] = useState(0);
+
   return (
     <>
-      <Counter />
+      <Counter key={resetKey} />
+      <button onClick={() => setResetKey(1)}>Reset Counter</button>
       <UserProvider>
         <Profile />
       </UserProvider>
