e2e976f565a988086a08339cd84ae251c6905d4f
khaingsuthiri <khaingsuthiri@gmail.com>
2026-03-11 14:35:44 +0630

    learning: tailwind and shadcn setup

---
+++ tailwind-test/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+}
+
+export default App;
