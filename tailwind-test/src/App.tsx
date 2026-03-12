f31db0bb2c0dd2eb62b5cdbc6cabcb28d6bb9df6
khaingsuthiri <khaingsuthiri@gmail.com>
2026-03-12 12:49:16 +0630

    learning: add alert component and icon package for testing

--- tailwind-test/src/App.tsx
+++ tailwind-test/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 import { Button } from "./components/ui/button";
+import { CheckCircle2Icon } from "lucide-react";
 
 function App() {
   return (
@@ -8,6 +10,15 @@ function App() {
         Hello world!
       </h1>
       <Button variant="outline">Button</Button>
+      <hr />
+      <Alert>
+        <CheckCircle2Icon />
+        <AlertTitle>Payment successful</AlertTitle>
+        <AlertDescription>
+          Your payment of $29.99 has been processed. A receipt has been sent to
+          your email address.
+        </AlertDescription>
+      </Alert>
     </>
   );
 }
